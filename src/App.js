4d7a58b2709afecc2bb78404804da335458dc829
DOUMBISSS <104175220+DOUMBISSS@users.noreply.github.com>
2022-09-06 14:20:12 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name , setName] = useState("");
+  const [userName , setUsername] = useState("");
+  const [followers , setFollowers] = useState("");
+  const [following , setFollowing] = useState("");
+  const [avatar , setAvatar] = useState("");
+  const [repos , setRepos] = useState("");
+  const [userInput , setUserInput] = useState("");
+  // const [data , setData] = useState("");
+  // const dispatch = useDispatch()
+  // const users = useSelector(state => state.userReducer.users);
+
+  
+  useEffect(() => {
+    fetch('https://api.github.com/users/example')
+    .then(response => response.json())
+    .then(data => {
+      setData(data);
+    })
+
+  }, [])
+
+  const setData = ({name ,
+    login ,
+    following ,
+    followers ,
+    avatar_url, 
+    public_repos 
+                  }) => {
+                        setName(name);
+                        setFollowers(followers);
+                        setFollowing(following);
+                        setUsername(login);
+                        setRepos(public_repos);
+                        setAvatar(avatar_url)
+  };
+  
+  const handeSearch =(event) =>{
+    setUserInput(event.target.value)
+  };
+
+  const handleSubmit = () =>{
+    fetch(`https://api.github.com/users/${userInput}`)
+    .then(response => response.json())
+    .then(data => {
+      setData(data);
+    })
+
+  };
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='container--box'>
+          <div className='container--header'>
+          <input type="text" placeholder='Github user' onChange={handeSearch} />
+          <button onClick={handleSubmit}>Search</button>
+          </div>
+          <div className='content'>
+            <div className='content--left'>
+                <div className='content--left--profil'>
+                    <img src={avatar} alt="" />
+                </div>
+            </div>
+            <div className='content--right'>
+               <div className='content--right--box--one'>
+                  <div className='name'>
+                  <h1>{name}</h1>
+                  </div>
+                  <div className='username'>
+                  <h1>{userName}</h1>
+                  </div>
+               </div>
+               <div className='content--right--box--two'>
+                 <div className='items' >
+                 <h3 className='item'>{followers} followers</h3>
+                  <h3 className='item'>{following} following</h3>
+                  <h3 className='item'>{repos}repos</h3>
+                 </div>
+               </div>
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
